f4556ea3c304df211173a4bd5e29c8c2d7584a74
harrychanhoyin95 <harrychanhoyin95@gmail.com>
2019-11-26 13:09:50 +0800

    Updates

---
+++ precache-manifest.7d4da7376e8a81261d30a967589bda38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e228462f54bb705e26a44eed946dbc",
+    "url": "/crypto-dashboard/index.html"
+  },
+  {
+    "revision": "617249ae9add84cb1562",
+    "url": "/crypto-dashboard/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "617249ae9add84cb1562",
+    "url": "/crypto-dashboard/static/js/2.1e07aa88.chunk.js"
+  },
+  {
+    "revision": "a25adc3d3fabeaa80394",
+    "url": "/crypto-dashboard/static/js/main.6d2b8dbb.chunk.js"
+  },
+  {
+    "revision": "6684e3524d93b0e8b2b2",
+    "url": "/crypto-dashboard/static/js/runtime~main.b72b2319.js"
+  }
+]);
